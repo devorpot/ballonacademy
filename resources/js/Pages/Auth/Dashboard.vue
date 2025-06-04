58aed72b471d1be514846d233a51b46436d3a079
Daniel <devops@orpot.com>
2025-06-03 21:42:18 -0600

    update deploy143d9

--- resources/js/Pages/Auth/Dashboard.vue
+++ resources/js/Pages/Auth/Dashboard.vue
@@ -7,7 +7,7 @@
  
     </form>
 
-    <p>Lorem ipsusada sm dolor sit amet consectetur adipisicing elit. Labore in, quibusdam amet deleniti, assumenda aliquid error sunt, qui totamasdasdsad saepe mollitia dignissimos aspernatur sint fugiat quis rem neque facere eaque.</p>
+    <p>Lorem ipsusada sm asdasd dolor sit amet consectetur adipisicing elit. Labore in, quibusdam amet deleniti, assumenda aliquid error sunt, qui totamasdasdsad saepe mollitia dignissimos aspernatur sint fugiat quis rem neque facere eaque.</p>
 
    </div>
 
